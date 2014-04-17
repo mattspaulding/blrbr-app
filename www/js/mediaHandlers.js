75c0c24523f140874683fb5eebbff72b1f4adac1
Matt Spaulding <mrmattspaulding@gmail.com>
2014-04-17 11:37:28 -0400

    Change to mp3.

--- www/js/mediaHandlers.js
+++ www/js/mediaHandlers.js
@@ -12,7 +12,7 @@ var recTime = 0;
 
 // for recording: do not specify any directory
 var mediaFileFullName = null; 
-var mediaRecFile = "myRecording100.wav";
+var mediaRecFile = "myRecording100.mp3";
 var checkFileOnly = false;
 var mediaFileExist = false;
 var myMediaState = {start: 1, 
