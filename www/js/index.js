cbe04a4ae47bfe77b3bf83861e578d70b38b8bd2
Matt Spaulding <mrmattspaulding@gmail.com>
2014-05-01 11:34:22 -0400

    Add textarea.

--- www/js/index.js
+++ www/js/index.js
@@ -63,10 +63,13 @@ var app = {
         		ref.close();
         		alert(event.url);
         		var p=event.url.split('username=')[1];
+        		var q=p.split('channel=')[1];
         		window.username=p.split('channel=')[0];
-        		window.channel=p.split('channel=')[1];
+        		window.channel=q.split('text=')[1];
+        		window.text=q.split('text=')[1];
         		alert(window.username);
         		alert(window.channel);
+        		alert(window.text);
         		window.location="create.html";
         	}
          	}
