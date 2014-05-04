f2a239af7e4a6e2933851c2443eafd496d30025d
Matt Spaulding <mrmattspaulding@gmail.com>
2014-05-04 14:08:05 -0400

    ugh

--- www/js/index.js
+++ www/js/index.js
@@ -17,67 +17,55 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+	// Application Constructor
+	initialize : function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents : function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicity call 'app.receivedEvent(...);'
+	onDeviceReady : function() {
+		app.receivedEvent('deviceready');
 
-        var ref = window.open('http://blrbrdev.azurewebsites.net', '_blank', 'toolbar=no,location=no');
+		var ref = window.open('http://blrbrdev.azurewebsites.net', '_blank', 'toolbar=no,location=no');
 
-        //ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-        ref.addEventListener('loadstart', function(event) { 
-        //alert('stop: ' + event.url); 
-        
-            
-          	if(event.url.indexOf('Blrb/Create') != -1)
-        	{
-        		ref.close();
-        		
-        		window.location= "create.html?"+event.url.split('?')[1];
-        	}
-       
-         	}
-        	);
-        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-        ref.addEventListener('exit', function(event) {  });
-        
-        StatusBar.hide();
-         navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		//ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+		ref.addEventListener('loadstart', function(event) {
+			//alert('stop: ' + event.url);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+			if (event.url.indexOf('Blrb/Create') != -1) {
+				ref.close();
 
-        console.log('Received Event: ' + id);
-    }
-    
-   
+				window.location = "create.html?" + event.url.split('?')[1];
+			}
+
+		});
+		ref.addEventListener('loaderror', function(event) {
+			alert('error: ' + event.message);
+		});
+		ref.addEventListener('exit', function(event) {
+		});
+
+		StatusBar.hide();
+		navigator.splashscreen.hide();
+	},
+	// Update DOM on a Received Event
+	receivedEvent : function(id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
+
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
+
+		console.log('Received Event: ' + id);
+	}
 };
-
-	function onBodyLoad() {
-		alert("app: "+app);
-		if(app==null)
-		{
-				alert("onbodyload");
-				app.initialize();
-			}}
-
-
